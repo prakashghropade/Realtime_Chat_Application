83013e1f2e161889f2a932ac1a2de52fcede48f9
prakashghorpade <prakashghorpade2001@gmail.com>
2024-07-16 14:47:22 +0530

    login and Sign up page completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,14 @@
-import { Button, Container, Paper, TextField, Typography } from '@mui/material'
+import { Avatar, Button, Container, IconButton, Paper, Stack, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
+import {CameraAlt as CameraAltIcon} from '@mui/icons-material'
+import { VisuallyHiddenInput } from '../components/styles/StyledComponents';
 
 const Login = () => {
  
 const [isLogin, setIsLogin] = useState(true);
 
 const toggleLogin  = () => {
-    setIsLogin(false);
+    setIsLogin((prev) => !prev);
 }
 
   return (
@@ -23,9 +25,9 @@ const toggleLogin  = () => {
     >
       
       {
-        isLogin ? <>
+        isLogin ? ( <>
         
-        <Typography varient="h5"></Typography>
+        <Typography varient="h5">Login</Typography>
         <form style={{
             width:"100%",
             marginTop: "1rem"
@@ -40,12 +42,68 @@ const toggleLogin  = () => {
             <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
 
             <Button  variant='text' color='secondary' fullWidth onClick={toggleLogin}>
-              Sign
+              Sign Up Instead
             </Button>
         </form>
         
         
-        </>: <span>Register</span>
+        </> ) : (
+          
+          <>
+             
+          <Stack position={"relative"}
+                 width={"10rem"}
+                 margin={"auto"}
+          >
+              <Avatar sx={{width: "10rem", height:"10rem", objectFit:"contain"}}/> 
+        
+
+          <IconButton  sx={{
+            position:"absolute",
+            bottom: "0",
+            right: "0",
+            color: "white",
+            bgcolor: "rgba(0, 0, 0, 0.5)",
+            ":hover": {
+              bgcolor: "rgba(0, 0, 0, 0.7)"
+            },
+          }}
+          component="label"
+          >
+            <>
+             <CameraAltIcon/>
+             <VisuallyHiddenInput type="file"/>
+            </>
+
+          </IconButton>
+          </Stack>
+        
+        <Typography varient="h5">Sign Up</Typography>
+        <form style={{
+            width:"100%",
+            marginTop: "1rem"
+        }}>
+            <TextField required fullWidth label="Name" margin='normal' variant='outlined'/>
+
+            <TextField required fullWidth label="Bio" margin='normal' variant='outlined'/>
+
+            <TextField required fullWidth label="Username" margin='normal' variant='outlined'/>
+
+            <TextField required fullWidth label="Password" type="password" margin='normal' variant='outlined'/>
+
+            <Button variant='contained' color='primary' type='submit' fullWidth>Sign Up</Button>
+
+
+            <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
+
+            <Button  variant='text' color='secondary' fullWidth onClick={toggleLogin}>
+              Login Instead
+            </Button>
+        </form>
+        
+        
+        </>
+        )
       }
 
     </Paper>
