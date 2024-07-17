1d2937643559adcc7c49f7c48d353555492db594
prakashghorpade <prakashghorpade2001@gmail.com>
2024-07-17 18:42:04 +0530

    userItem component added

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { Dialog, DialogTitle, InputAdornment, List, ListItem, ListItemText, Stack, TextField } from '@mui/material'
+import React from 'react';
+import {useInputValidation} from "6pp"
+import { Search as SearchIcon } from '@mui/icons-material';
+import UserItem from '../shared/UserItem';
+
+const users = [1, 2, 3];
 
 const Search = () => {
-  return (
-    <div>
-      Search
-    </div>
-  )
+
+  const  search = useInputValidation("");
+  return <Dialog open>
+    
+    <Stack p={'2rem'} direction={"column"} width={"25rem"}>
+           <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+           <TextField  
+           
+           label="" 
+           value={search.value} 
+           onChange={search.changeHandler}
+           variant='outlined'
+           size='small'
+           InputProps={{
+            startAdornment:(
+              <InputAdornment position='start'>
+               <SearchIcon/>
+              </InputAdornment>
+            )
+           }}
+           />
+
+
+       <List>
+            {
+              users.map((user) => {
+                 <UserItem/>
+              })
+            }
+       </List>
+
+
+    </Stack>
+
+  </Dialog>
 }
 
 export default Search
